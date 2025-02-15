80bf5adcdd02acbbdec06bdbdc4ec9b738b2b506
Colby Hemond <hemond.colby@gmail.com>
2025-02-15 06:53:08 -0500

    fa

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,24 +2,14 @@ import Navbar from "@/components/layout/Navbar";
 import "./globals.css";
 import Footer from "@/components/layout/Footer";
 import FathomAnalytics from "@/components/FathomAnalytics";
-import { client } from "@/sanity/client";
 
 export const metadata = {
   title: "Quickstart Site", //TODO - connect to sanity
   description: "My Website", //TODO - connect to sanity
 };
 
-
-const SETTINGS_QUERY = `*[_type == "settings"][0]`;
-
-const options = { next: { revalidate: 30 } };
-
-
 export default async function RootLayout({ children }) {
 
-  // const settings = await client.fetch(SETTINGS_QUERY, {}, options)
-
-  
   return (
     <html lang="en">
       <FathomAnalytics />
